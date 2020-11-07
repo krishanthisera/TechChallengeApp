eff7fc94ffb024c4278c9b092f3e584decdc7a0a
krishanthisera <krishees@gmail.com>
2020-11-08 09:09:04 +1030

    EBS volume policy

---
+++ iac/ebs-volumes.tf
@@ -0,0 +1,33 @@
+resource "aws_iam_policy" "eks_ebs_policy" {
+  name = "Amazon_EBS_CSI_Driver"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:AttachVolume",
+        "ec2:CreateSnapshot",
+        "ec2:CreateTags",
+        "ec2:CreateVolume",
+        "ec2:DeleteSnapshot",
+        "ec2:DeleteTags",
+        "ec2:DeleteVolume",
+        "ec2:DescribeInstances",
+        "ec2:DescribeSnapshots",
+        "ec2:DescribeTags",
+        "ec2:DescribeVolumes",
+        "ec2:DetachVolume"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+POLICY
+}
+resource "aws_iam_role_policy_attachment" "eks_ebs_driver" {
+  policy_arn = aws_iam_policy.eks_worknode_ebs_policy.arn
+  role       = aws_iam_role.eks_worknode.name
+}
